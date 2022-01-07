e585d89060dc89d1d3aa966cff81d744d2e6c3e1
unknown <alonso.martinez@uabc.edu.mx>
2022-01-07 15:29:43 -0800

    first commit

---
+++ src/Components/ScoreBoard.js
@@ -0,0 +1,12 @@
+const ScoreBoard =({puntos}) =>{
+    return(
+            <div className="score-board">
+                <h2>
+                    Score: {puntos}
+                </h2>
+            </div>
+
+    )
+}
+
+export default ScoreBoard
